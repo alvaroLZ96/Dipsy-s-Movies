7b109666d1c6504d679354374be1c1c280fa5b54
alvaroLZ96 <alvarost54@gmail.com>
2023-03-30 17:42:46 +0200

    url server

--- app/src/services/API.js
+++ app/src/services/API.js
@@ -14,5 +14,5 @@ const apiHeaders = {
 export const API = axios.create({
   headers: apiHeaders,
   timeout: 6000,
-  baseURL: 'http://localhost:6060/api'
+  baseURL: 'http://dipsy-s-movies-production.up.railway.app'
 })
